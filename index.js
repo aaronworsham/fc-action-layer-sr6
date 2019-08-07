9c0d2f950fc1acb862419681f00ae6eb8e19486e
Aaron <aaronworsham@gmail.com>
2019-08-07 09:43:30 -0400

    net hits

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 'use strict';
 
-
-module.exports.roll = roll;
+module.exports.rollForHits = rollForHits;
+module.exports.reroll = reroll;
 const { DiceRoller } = require('rpg-dice-roller');
 const diceRoller = new DiceRoller();
 
-function roll(max){
-	return diceRoller.roll('1d6');
+function rollForHits(options){
+	/*
+	options
+		.threshold - number to judge hits by
+		.explode - does it explode on 6, default is false
+	*/
+
+	var diceRoll
+	if (options.explode) {
+		diceRoll = diceRoller.roll(options.poolSize + 'd6!');
+	}
+	else{
+		diceRoll = diceRoller.roll(options.poolSize + 'd6');
+	}
+	diceRoll.hits = countHits(diceRoll, options);
+	return diceRoll
+}
+
+function countHits(diceRoll, options){
+	var hits = 0;
+	for (var i = diceRoll.rolls[0].length - 1; i >= 0; i--) {
+		if (diceRoll.rolls[0][i] > options.threshold) {
+			hits++
+		}
+	}
+	return hits
+}
+
+function reroll(rerollArray, diceRoll){
+	var newRolls = diceRoller.roll(rerollArray.length + 'd6');
+	var x = 0;
+	for (var i in rerollArray){
+		console.log("Updating " + diceRoll.rolls[0][rerollArray[i]] + " to " + newRolls.rolls[0][x])
+		diceRoll.rolls[0][rerollArray[i]] = newRolls.rolls[0][i]
+		x++;
+	}
+	return diceRoll;
 }
--- test/test.js
+++ test/test.js
@@ -3,10 +3,61 @@
 var expect = require('chai').expect;
 var actionLayer = require('../index');
 
-describe('#rollDice', function() {
-    it('should roll dice', function() {
-        var result = actionLayer.roll(6);
-        console.log(result);
+describe('Pool of Dice', function() {
+	describe('Standard Pool', function(){
+    it('should roll a pool of 6 dice', function() {
+    		var options = {explode: false, threshold: 4, poolSize: 6}
+        var result = actionLayer.rollForHits(options);
         expect(result.rolls[0][0]).to.be.below(7);
+        expect(result.rolls[0].length).to.equal(6)
+    });	
+     it('will have hits after 100 rolls', function(){
+    	var diceRolled = 0;
+			var options = {explode: false, threshold: 4, poolSize: 6}
+    	for (var i = 100; i >= 0; i--) {
+        var result = actionLayer.rollForHits(options);
+    		diceRolled += result.hits
+    	}
+    	expect(diceRolled).to.be.above(0);
+    })	
+	})
+	describe('Exploding Pool', function(){
+    it('should roll an exploding pool of 6 dice', function() {
+    		var options = {explode: false, threshold: 4, poolSize: 6}
+        var result = actionLayer.rollForHits(options);
+        expect(result.rolls[0][0]).to.be.below(7);
+        expect(result.rolls[0].length).to.be.above(5)
     });
+    it('will explode after 100 rolls', function(){
+    	var diceRolled = 0;
+    	var options = {explode: false, threshold: 4, poolSize: 6}
+    	for (var i = 100; i >= 0; i--) {
+        var result = actionLayer.rollForHits(options);
+    		diceRolled += result.rolls[0].length
+    	}
+    	expect(diceRolled).to.be.above(600);
+    })	
+	})
+	describe('Rerolling dice in Pool', function(){
+		it('will reroll one of the dice', function(){
+  		var options = {explode: false, threshold: 4, poolSize: 6}
+      var result = actionLayer.rollForHits(options);
+      var oldResult = result.rolls[0].slice(0)  //clone the value of the array
+      console.log(oldResult)
+			var newResult = actionLayer.reroll([0], result)		
+			console.log(newResult);
+			console.log(result);
+		});
+		it('will reroll three of the dice', function(){
+  		var options = {explode: false, threshold: 4, poolSize: 6}
+      var result = actionLayer.rollForHits(options);
+      var oldResult = result.rolls[0].slice(0)  //clone the value of the array
+      console.log(oldResult)
+			var newResult = actionLayer.reroll([0,2,4], result)		
+			console.log(newResult);
+			console.log(result);
+		})
+	})
 });
+
+
